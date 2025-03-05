fc881abc89887d64ff3244ac0aa2d812eb3ce806
luimungar3 <91976477+luimungar3@users.noreply.github.com>
2025-03-05 19:49:26 +0100

    Create TareaController.java

---
+++ src/main/java/com/tarea/controller/TareaController.java
@@ -0,0 +1,12 @@
+package com.ejemplo.listadetareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeTareasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaDeTareasApplication.class, args);
+    }
+}
